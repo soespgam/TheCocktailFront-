505aaa957b272cf773f9a91e380e1e8bf5732cad
soespgam <soespgam@gmail.com>
2024-08-07 10:15:07 -0500

    se crean carpetas correspondientes y rutas para pagina home

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './page/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    NavbarComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    HttpClientModule
+  ],
+  exports:[
+    NavbarComponent
+  ]
+})
+export class HomeModule { }
